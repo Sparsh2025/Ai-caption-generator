a33155ac74c82bc3ff4935ac7930c9c43884aa7f
Sparsh2025 <sparshchoubey19@gmail.com>
2025-11-04 15:44:25 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const cors=require('cors')
 const app=express()
 app.use(
   cors({
-    origin: "https://ai-caption-t2u1.onrender.com"     
+    origin: "https://ai-caption-t2u1.onrender.com" || "http://localhost:5173",    
   })
 );
 app.use(express.json())
